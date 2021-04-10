63506faec513d791a0498d84b3a0e772533c2594
aurelia Segarra <av.segarra@gmail.com>
2021-04-10 15:43:39 +0200

    :lipstick:updating App component and Board

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,16 @@
 // == Import npm
 import React from 'react';
+import Board from 'src/components/Board';
 
 // == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <h1 className="title">Tic Tac Toe</h1>
+    <h2 className="subtitle">The Game 🤖</h2>
+    <Board />
   </div>
 );
 
---
+++ src/components/Board/index.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const Board = () => (
+  <div className="board">
+    <h1>je suis le board</h1>
+  </div>
+);
+
+// == Export
+export default Board;
