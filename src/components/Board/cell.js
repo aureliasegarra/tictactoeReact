c811a658c0ac13bfe8c99da71e11da76e25f496f
aurelia Segarra <av.segarra@gmail.com>
2021-04-10 17:39:18 +0200

    :construction: Cell component static

---
+++ src/components/Board/cell.js
@@ -0,0 +1,15 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const Cell = () => (
+  <div className="cell">
+    <h1>je suis une case</h1>
+  </div>
+);
+
+// == Export
+export default Cell;
