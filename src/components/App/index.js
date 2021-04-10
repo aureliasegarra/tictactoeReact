17055f2b0eaf46c98d4c5787b1a1ce20c040b711
aurelia Segarra <av.segarra@gmail.com>
2021-04-10 18:24:03 +0200

    :wrench: func handleOnClick ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,9 +9,15 @@ import Cells from 'src/components/Cells';
 // == Composant
 const App = () => {
   const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+  const [xTurn, setXTurn] = useState(true);
 
-  const handleOnClick = () => {
-
+  const handleOnClick = (i) => {
+    const newCells = [...cells];
+    // si la case est cochée d'un signe alors on arrête et on passe à l'autre joueur
+    if (newCells[i]) return;
+    newCells[i] = xTurn ? 'X' : 'O';
+    setCells(newCells);
+    setXTurn(!xTurn);
   };
 
   return (
